a5e31968befad4769809892881babb85982a9930
junior42fr <66255949+junior42fr@users.noreply.github.com>
2022-11-28 07:55:21 +0100

    ajout des droits exécution pour loki.py

--- script/forensic.sh
+++ script/forensic.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 packages_depot=(
+ "build-essential"
  "pff-tools"
  "binwalk"
  "mariadb-server"
+ "libdistorm3-dev"
+ "yara"
+ "libraw1394-11"
+ "libcapstone-dev"
+ "capstone-tool"
+ "tzdata"
+ "python2"
+ "python2.7-dev"
+ "libpython2-dev"
 )
 
-SIFT_CLI_LINUX="https://github.com/sans-dfir/sift-cli/releases/download/v1.13.1/sift-cli-linux"
+#SIFT_CLI_LINUX="https://github.com/sans-dfir/sift-cli/releases/download/v1.13.1/sift-cli-linux"
+SIFT_CLI_LINUX="https://github.com/teamdfir/sift-cli/releases/download/v1.14.0-rc1/sift-cli-linux"
+
 CAPA="https://github.com/mandiant/capa/releases/download/v4.0.1/capa-v4.0.1-linux.zip"
 DUMPZILLA="http://www.dumpzilla.org/dumpzilla.py"
+FENRIR="https://github.com/Neo23x0/Fenrir"
 FLOSS="https://github.com/fireeye/flare-floss/releases/download/v1.7.0/floss-v1.7.0-linux.zip"
+LOKI="https://github.com/Neo23x0/Loki"
+LOKI_SIGNATURE="https://github.com/Neo23x0/signature-base"
 SIGMA="https://github.com/SigmaHQ/sigma"
 VOLATILITY3="https://github.com/volatilityfoundation/volatility3.git"
 ZIRCOLITE="https://github.com/wagga40/Zircolite"
 ZIRCOLITE_DOC="https://github.com/wagga40/Zircolite/raw/master/docs/Zircolite_manual.pdf"
-FENRIR="https://github.com/Neo23x0/Fenrir"
-LOKI="https://github.com/Neo23x0/Loki"
-LOKI_SIGNATURE="https://github.com/Neo23x0/signature-base"
 
 Etape10_disableautomaticupdate(){
 	echo "---------------------------------------------------------------------"
@@ -76,10 +88,11 @@ Etape50_install_unpacked_tools(){
 	echo "---------------------------------------------------------------------"
 
 	sudo mkdir /TOOLS
-	cd /TOOLS
+	ln -s /TOOLS $(pwd)/Bureau/Outils
 
 	#Capa
 	echo " >>>>>>  Installation de Capa "
+	cd /TOOLS
 	#Récupération de CAPA
 	sudo wget $CAPA
 	#Mise en place de CAPA
@@ -94,6 +107,7 @@ Etape50_install_unpacked_tools(){
 
 	#Dumpzilla
 	echo " >>>>>>  Installation de Dumpzilla "
+	cd /TOOLS
 	sudo wget $DUMPZILLA
 	arrDUMP=(${DUMPZILLA//// })
 	sudo chmod +x ${arrDUMP[-1]}
@@ -102,8 +116,18 @@ Etape50_install_unpacked_tools(){
     echo "extract all forensic interesting information of Firefox, Iceweasel and Seamonkey browsers to be analyzed" | sudo tee -a /TOOLS/README_for_tools.txt
 	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
 
+	#Fenrir
+	echo " >>>>>>  Installation de Fenrir "
+	cd /TOOLS
+	sudo git clone $FENRIR
+	echo "Fenrir" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "------" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "Simple Bash IOC Scanner" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
+
 	#Floss
 	echo " >>>>>>  Installation de Floss "
+	cd /TOOLS
 	sudo wget $FLOSS
 	arrFLOSS=(${FLOSS//// })
 	sudo unzip ${arrFLOSS[-1]}
@@ -113,31 +137,65 @@ Etape50_install_unpacked_tools(){
 	echo "FLARE Obfuscated String Solver (FLOSS) - Automatically extract obfuscated strings from malware. " | sudo tee -a /TOOLS/README_for_tools.txt
 	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
 
+	#Loki
+	echo " >>>>>>  Installation de Loki "
+	cd /TOOLS
+	sudo git clone $LOKI
+	sudo pip3 install -r ./Loki/requirements.txt
+	cd Loki
+	chmod +x loki.py
+	echo " >>>>>>  Mise en place des signatures pour Loki "
+	sudo git clone $LOKI_SIGNATURE
+	sudo python3 loki-upgrader.py
+	cd ..
+	echo "Loki" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "----" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "Simple IOC and Incident Response Scanner" | sudo tee -a /TOOLS/README_for_tools.txt
+	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
+
 	#Oletools
 	echo " >>>>>>  Installation de Oletools "
-		#Désactivation de la demande de KDE Wallet Service
+	cd /TOOLS
+	#Désactivation de la demande de KDE Wallet Service
 	sudo python -m keyring --disable
-	sudo pip install oletools
+	sudo pip3 install oletools
 
 	#RegRippy
 	echo " >>>>>>  Installation de RegRippy "
-	sudo pip install regrippy
+	cd /TOOLS
+	sudo pip3 install regrippy
 
 	#SigmaHQ/sigma
 	echo " >>>>>>  Installation de Sigma "
-	sudo pip install ruamel.yaml
+	cd /TOOLS
+	sudo pip3 install ruamel.yaml
 	sudo git clone $SIGMA
 	echo "Sigma" | sudo tee -a /TOOLS/README_for_tools.txt
 	echo "-----" | sudo tee -a /TOOLS/README_for_tools.txt
 	echo "Generic Signature Format for SIEM Systems " | sudo tee -a /TOOLS/README_for_tools.txt
 	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
 
+	#Volatility2
+	echo " >>>>>>  Installation de Volatility2 "
+	cd
+	curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+	sudo python2 get-pip.py
+	rm -f get-pip.py
+	sudo python2 -m pip install -U setuptools wheel
+	python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+	sudo python2 -m pip install yara
+	sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+	python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
+	sudo ln -s $(pwd)/.local/bin/vol.py /usr/bin/vol.py
+
 	#Récupération Github Volatility3
 	echo " >>>>>>  Installation de Volatility3 "
+	cd /TOOLS
 	sudo git clone $VOLATILITY3
 
 	#Zircolite
 	echo " >>>>>>  Installation de Zircolite "
+	cd /TOOLS
 	sudo /usr/bin/python3 -m pip install --upgrade pip
 	sudo pip3 install tqdm
 	sudo git clone $ZIRCOLITE
@@ -151,27 +209,6 @@ Etape50_install_unpacked_tools(){
 	#Création des règles SIGMA dans le dossier Zircolite
 	sudo ./sigma/tools/sigmac -t sqlite -c ./sigma/tools/config/generic/sysmon.yml -c ./sigma/tools/config/generic/powershell.yml -c ./sigma/tools/config/zircolite.yml -d ./sigma/rules/windows/ --output-fields title,id,description,author,tags,level,falsepositives,filename,status --output-format json -r -o ./Zircolite/rules_sysmon.json --backend-option table=logs
 	sudo ./sigma/tools/sigmac -t sqlite -c ./sigma/tools/config/generic/windows-audit.yml -c ./sigma/tools/config/generic/powershell.yml -c ./sigma/tools/config/zircolite.yml -d ./sigma/rules/windows/ --output-fields title,id,description,author,tags,level,falsepositives,filename,status --output-format json -r -o ./Zircolite/rules_generic.json --backend-option table=logs
-
-	#Fenrir
-	echo " >>>>>>  Installation de Fenrir "
-	sudo git clone $FENRIR
-	echo "Fenrir" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "------" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "Simple Bash IOC Scanner" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
-
-	#Loki
-	echo " >>>>>>  Installation de Loki "
-	sudo git clone $LOKI
-	sudo pip3 install -r ./Loki/requirements.txt
-	cd Loki
-	echo " >>>>>>  Mise en place des signatures pour Loki "
-	sudo git clone $LOKI_SIGNATURE
-	cd ..
-	echo "Loki" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "----" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "Simple IOC and Incident Response Scanner" | sudo tee -a /TOOLS/README_for_tools.txt
-	echo "" | sudo tee -a /TOOLS/README_for_tools.txt
 
 	echo "---------------------------------------------------------------------"
 	echo "OUTILS INDEPENDANTS INSTALLES"
